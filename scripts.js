5c028a401824a022a8f21e27406e85492ee4f707
khorne flakes <khorneflakesdev@gmail.com>
2022-11-19 21:42:04 -0400

    burger-menu

--- scripts.js
+++ scripts.js
@@ -126,3 +126,9 @@ document.addEventListener('DOMContentLoaded' , () => {
 
 });
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+    // var elems = document.querySelectorAll('.sidenav');
+    // var instances = M.Sidenav.init(elems);
+  });
