c69c4fc688e4249139ba8e8a3f4ade0a30b51baf
erick <98509698+khorneflakes-dev@users.noreply.github.com>
2022-11-22 04:43:06 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -133,9 +133,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
-// window.onload = function() {
-//     if(!window.location.hash) {
-//         window.location = window.location + '#loaded';
-//         window.location.reload();
-//     }
-// }
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
